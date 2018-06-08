e7aa53fad486c97ddbb656598199954ece291293
Andreas Schröpfer <andreas@apxp.info>
2018-06-08 13:20:40 +0200

    json beispiel

--- 1_Codebeispiele/020_Grundlagen_Go/06_json/04_bsp/main.go
+++ 1_Codebeispiele/020_Grundlagen_Go/06_json/04_bsp/main.go
@@ -1,13 +1,13 @@
 package main
 
 type Foo struct {
-	A string
-	B string
+	A string `json:"a,omitempty"`
+	B string `json:"b,omitempty"`
 }
 
 type Bar struct {
-	F1 *Foo
-	F2 Foo
+	F1 *Foo `json:"f_1,omitempty"`
+	F2 Foo  `json:"f_2,omitempty"`
 }
 
 func main() {
